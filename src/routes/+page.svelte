9582bd5911d0f005048a5fbe901d22af8794bf30
William <21cenweihong@gmail.com>
2024-07-29 23:54:10 -0700

    updated auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,16 @@
     import { createAuth0Client } from '@auth0/auth0-spa-js';
     import { onMount } from 'svelte';
     import { goto } from '$app/navigation';
+    import { enhance } from '$app/forms'
+
+
     
 	onMount(async () => {
-        // goto('/dashboard');
+        // goto('/auth');
 	});
 </script>
 
 <div>
     <h1 class="text-white text-4xl font-bold">Welcome</h1>
 </div>
+<a href="/auth">login</a>
